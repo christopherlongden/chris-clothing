ba56daa49cd43605e6474f7759fe5f9e9911191d
Chris Longden <longden@longden.net>
2020-02-22 02:07:57 +0100

    added collections overview component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
@@ -11,7 +11,7 @@ const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['cart']
-}
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
