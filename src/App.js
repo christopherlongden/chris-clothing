81b4949eb40e74fab59bff7d7dcd314730e7ecd9
Chris Longden <longden@longden.net>
2020-02-23 11:39:09 +0100

    sync project with master

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
+//import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -36,7 +38,10 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
-    });
+
+      //addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})));
+
+    }, error => console.log(error));
   }
 
   componentWillUnmount() {
@@ -70,6 +75,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
+  //collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
